d51bb6895f644c03a4e58f33c741c4d785e71713
resie001 <aderesie.m.s@gmail.com>
2019-09-27 22:18:44 +0700

    beta

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Submission 2 Microsoft Azure</title>
     <script src="{{asset('/js/jquery-3.4.1.min.js')}}"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <h1>
-        Hello World
-    </h1>
+    <p>Pilih gambar yang akan di Analisa</p>
+    <input type="file" name="image" id="image"><br>
+    <input type="submit" value="Upload" name="upload" class="btn btn-primary">
 </body>
 </html>
