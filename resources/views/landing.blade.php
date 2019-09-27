b53e3f0cec63cf4727bd11bdb39747ef71b99be7
resie001 <aderesie.m.s@gmail.com>
2019-09-27 22:39:40 +0700

    beta 2.0

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,6 +7,12 @@
     <title>Submission 2 Microsoft Azure</title>
     <script src="{{asset('/js/jquery-3.4.1.min.js')}}"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        * {
+            padding: 16px;
+            margin: 16px;
+        }
+    </style>
 </head>
 <body>
     <p>Pilih gambar yang akan di Analisa</p>
